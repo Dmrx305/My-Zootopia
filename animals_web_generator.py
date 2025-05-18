bb809b84d6a400ef6cb97812d0667152e0840bf5
Denny <den.marx@web.de>
2025-05-18 14:33:24 +0200

    improved code-formatting,added further details for html display

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -4,21 +4,27 @@ def load_data(file_path):
     """ Loads a JSON file """
     with open(file_path, "r") as handle:
         return json.load(handle)
-
-
 animals_data = load_data('animals_data.json')
-output = ""
 
-for animal in animals_data:
+
+def serialize_animal(animal_obj):
+    """ Serializes a list of animals """
+    output = ""
     output += '<li class="cards__item">'
-    output += '<div class="card__title">'f"{animal['name']}</div>"
+    output += '<div class="card__title">'f"{animal_obj['name']}</div>"
     output += '<p class="card__text">'
-    output += '<strong>Diet: </strong>'f"{animal["characteristics"]["diet"]}<br/>\n"
-    output += '<strong>Location: </strong>'f"{animal["locations"][0]}<br/>\n"
-    if "type" in animal["characteristics"]:
-        output += '<strong>Type: </strong>'f"{animal["characteristics"]["type"]}<br/>\n"
+    output += '<strong>Scientific Name: </strong>'f"{animal_obj["taxonomy"]['scientific_name']}<br/>\n"
+    output += '<strong>Diet: </strong>'f"{animal_obj["characteristics"]["diet"]}<br/>\n"
+    output += '<strong>Location: </strong>'f"{animal_obj["locations"][0]}<br/>\n"
+    if "type" in animal_obj["characteristics"]:
+        output += '<strong>Type: </strong>'f"{animal_obj["characteristics"]["type"]}<br/>\n"
         output += '</p>'
     output += "</li>"
+    return output
+
+output = ''
+for animal_obj in animals_data:
+    output += serialize_animal(animal_obj)
 
 with open("animals_template.html", "r") as template:
     html_content = template.read()
